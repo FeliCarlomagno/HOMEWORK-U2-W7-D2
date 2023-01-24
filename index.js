9a846ef454d0343f57af3e5e55e3ef55850ad81f
FeliCarlomagno <feliciano.carlomagno@libero.it>
2023-01-24 18:16:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ class Person {
 
   static showAge(personA, personB, personC) {
     if (personA.age > personB.age && personA.age > personC.age) {
-      return `L'eta di ${persone[0].name} è maggiore dell'eta di ${persone[1].name} e di ${persone[2].name} !`;
+      return `L'eta di ${persone[0].name} è maggiore dell'eta di ${persone[1].name} e di ${persone[2].name}!`;
     } else if (personB.age > personA.age && personB.age > personC.age) {
-      return `L'eta di ${persone[1].name} è maggiore dell'eta di ${persone[0].name} e di ${persone[2].name} !`;
+      return `L'eta di ${persone[1].name} è maggiore dell'eta di ${persone[0].name} e di ${persone[2].name}!`;
     } else if (personC.age > personA.age && personC.age > personB.age) {
-      return `L'eta di ${persone[2].name} è maggiore dell'eta di ${persone[0].name} e di ${persone[1].name} !`;
+      return `L'eta di ${persone[2].name} è maggiore dell'eta di ${persone[0].name} e di ${persone[1].name}!`;
     } else {
       return "Le età di entrambe le persone sono uguali!";
     }
@@ -18,9 +18,9 @@ class Person {
 }
 
 let persone = [
-  new Person("Andrea", 19),
+  new Person("Andrea", 39),
   new Person("Luca", 23),
-  new Person("Gianmarco", 50),
+  new Person("Gianmarco", 30),
 ];
 
 console.log(Person.showAge(persone[0], persone[1], persone[2]));
